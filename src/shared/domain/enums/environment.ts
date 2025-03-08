36bd9cdfe24be238b4869aff7a7e6a4116b69adb
maticapuano <maticapuano@users.noreply.github.com>
2025-03-08 14:19:12 +0000

    ci(lint): format code

--- src/shared/domain/enums/environment.ts
+++ src/shared/domain/enums/environment.ts
@@ -1,5 +1,5 @@
 export enum Environment {
   TEST = 'test',
   DEVELOPMENT = 'development',
-  PRODUCTION = 'production'
+  PRODUCTION = 'production',
 }
--- src/shared/domain/exceptions/invalid-argument.ts
+++ src/shared/domain/exceptions/invalid-argument.ts
@@ -2,7 +2,10 @@ import { ExceptionCode } from '../enums/exception-code';
 import { BadRequestException } from './bad-request';
 
 export class InvalidArgumentException extends BadRequestException {
-  public constructor(message: string, code: ExceptionCode = ExceptionCode.INVALID_ARGUMENT) {
+  public constructor(
+    message: string,
+    code: ExceptionCode = ExceptionCode.INVALID_ARGUMENT,
+  ) {
     super(message, code);
   }
 }
